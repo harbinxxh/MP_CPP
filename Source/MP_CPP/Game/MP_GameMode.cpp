cd84baf9053e0cf07c222e0529aae8b5b956015e
xuxh <harbinxxh@gmail.com>
2026-05-25 13:19:03 +0800

    The Game Mode UEC++

---
+++ Source/MP_CPP/Game/MP_GameMode.cpp
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MP_GameMode.h"
+
+AMP_GameMode::AMP_GameMode()
+{
+	// 游戏是否应在第一位玩家登录时立即开始。此设置会影响 ReadyToStartMatch 的默认行为。
+	bDelayedStart = true;
+	MatchStartDelay = 5.f;
+}
+
+void AMP_GameMode::StartMatch()
+{
+	Super::StartMatch();
+	
+	GEngine->AddOnScreenDebugMessage(
+	-1,
+	5.f,
+	FColor::Orange,
+	FString::Printf(TEXT("The match has started."))
+	);
+	
+	
+}
+
+void AMP_GameMode::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	GetWorldTimerManager().SetTimer(MatchStartTimer, this, &AMP_GameMode::StartDelayFinished, MatchStartDelay,false);
+}
+
+void AMP_GameMode::StartDelayFinished()
+{
+	StartMatch();
+}
---
+++ Source/MP_CPP/Game/MP_GameMode.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "MP_GameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MP_CPP_API AMP_GameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+public:
+	AMP_GameMode();
+	virtual void StartMatch() override;
+	
+protected:
+	virtual void BeginPlay() override;
+	
+private:
+	// 定时器句柄
+	FTimerHandle MatchStartTimer;
+	
+	UPROPERTY(EditDefaultsOnly)
+	float MatchStartDelay;
+	
+	void StartDelayFinished();
+};
